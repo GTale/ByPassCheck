f609630deefddbe4374ffb3a72087e6d62668ce6
C君 <dgut2331@gmail.com>
2020-09-13 13:02:43 +0800

    update

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: ./typecho/start.sh
--- typecho/start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-clear
-wget -o typecho.tar.gz https://github.com/typecho/typecho/releases/download/v1.1-17.10.30-release/1.1.17.10.30.-release.tar.gz
-mkdir typecho && tar -xvf typecho.tar.gz -C typecho && cd typecho
